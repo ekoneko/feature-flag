a6cb102effa60f5cb595552f8cdc08b48bd5878c
ekoneko <9@ikaze.me>
2017-07-20 18:42:31 +0800

    feat: read .babelrc don't set options

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const getOptions = require('./lib/getOptions');
 const parse = require('./lib/parse');
 const detect = require('./lib/detect');
 const transform = require('./lib/transform');
 
-module.exports = function (text, rules, options) {
-    const flags = parse(text, options)
+module.exports = function (text, rules, rawOptions) {
+    const options = rawOptions ? rawOptions : getOptions();
+    const flags = parse(text, options);
     const flagScopes = detect(flags, rules);
     return transform(text, flagScopes);
 }
--- test/simple.js
+++ test/simple.js
@@ -2,15 +2,26 @@ const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
 
+const getOptions = require('../lib/getOptions');
 const parse = require('../lib/parse');
 const detect = require('../lib/detect');
 const transform = require('../lib/transform');
 const featureFlag = require('../index');
 
+require.main.filename = require.resolve('./assets/simple.js')
+
 const simplePath = path.resolve(__dirname, './assets/simple.js');
 const text = fs.readFileSync(simplePath).toString();
+const babelOptions = getOptions();
 
 describe('simple', () => {
+    it ('getOptions', done => {
+        if (!babelOptions.presets || !babelOptions.plugins) {
+          done('parse .babelrc failed');
+        }
+        done();
+    });
+
     it ('A && B && !C', done => {
         const rules = {
             A: true,
@@ -18,7 +29,7 @@ describe('simple', () => {
             C: false
         };
 
-        const flags = parse(text);
+        const flags = parse(text, babelOptions);
         if (_.get(flags, 'start[0].value').indexOf('@flag_start') !== 0) {
             return done('@flag_start not match');
         }
@@ -43,7 +54,7 @@ describe('simple', () => {
             C: true
         };
 
-        const flags = parse(text)
+        const flags = parse(text, babelOptions)
         if (_.get(flags, 'start[0].value').indexOf('@flag_start') !== 0) {
             return done('@flag_start not match');
         }
